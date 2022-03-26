c7558b13b043fe07ec37d4de3020481026a81564
Ilnur <nabiulli@mail.ru>
2022-03-26 20:24:51 +0500

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/webemployee/WebEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/webemployee/WebEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
